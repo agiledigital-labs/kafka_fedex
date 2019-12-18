81cf385628d387564e74f4974933ff8d7d87d20e
sarker1999 <sxsarker@agiledigital.com.au>
2019-12-19 10:27:08 +1100

    fixed kafka local consumer

--- kafka-node/kafka-consumer.js
+++ kafka-node/kafka-consumer.js
@@ -1,59 +1,91 @@
-/*
-var kafka = require('kafka-node'),
-    Consumer = kafka.Consumer,
-    client = new kafka.KafkaClient("my-cluster-kafka-bootstrap:9092/"),
-    consumer = new Consumer(
-         client,
-        [
-              { topic: 'fedex', partition: 0, offset: 0 }
-        ],
-        { fromOffset: 'earliest' }
-    );
-
-console.log('something is happpening');
-
-
-consumer.on('message', function (message)
-{
- console.log(message);
-});
-
-consumer.on('error', function (err)
-{
-console.log('ERROR: ' + err.toString());
-});
-*/
-var kafka = require('kafka-node');
-var Consumer = kafka.Consumer;
-var Offset = kafka.Offset;
-var Client = kafka.KafkaClient;
-var topic = 'fedex';
-var client = new Client(process.env.ZOOKEEPER_PEERS);
-client.on('ready', function () {
-    console.log('client is ready');
-});
-var topics = [
-    { topic: topic, partition: 0, offset: 0 },
-];
-var options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMinBytes: 1, fetchMaxBytes: 1024 * 1024, fromOffset: true };
-var consumer = new Consumer(client, topics, options);
-var offset = new Offset(client);
-consumer.on('message', function (message) {
-    console.log(message);
-});
-consumer.on('error', function (err) {
-    console.log('error', err);
-});
-/*
-* If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset
-*/
-consumer.on('offsetOutOfRange', function (topic) {
-    topic.maxNum = 2;
-    offset.fetch([topic], function (err, offsets) {
-        if (err) {
-            return console.error(err);
-        }
-        var min = Math.min(offsets[topic.topic][topic.partition]);
-        consumer.setOffset(topic.topic, topic.partition, min);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-});
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+module.exports = function (context) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            /*
+                var kafka = require('kafka-node');
+                var Consumer = kafka.Consumer;
+                var Offset = kafka.Offset;
+                var Client = kafka.KafkaClient;
+                var topic = 'my-topic';
+            
+                //console.log('Your typescript works');
+                var client = new Client();         //(process.env.ZOOKEEPER_PEERS);
+                client.on('ready',function() {
+                  //console.log('client is ready');
+                  //console.log(client);
+                  //console.log('End of client');
+                });
+            
+                var topics = [
+                    {topic: topic, partition: 0,offset:0},
+                ];
+                var options = { autoCommit: false, fetchMaxWaitMs: 1000,fetchMinBytes: 1, fetchMaxBytes: 1024 * 1024 , fromOffset:true};
+            
+                var consumer = new Consumer(client, topics, options);
+                var offset = new Offset(client);
+            
+                //console.log('Something is going on');
+            
+                consumer.on('message', function (message) {
+                  //console.log(message);
+                });
+            
+                consumer.on('error', function (err) {
+                  //console.log('error', err);
+                });
+            
+            
+                consumer.on('offsetOutOfRange', function (topic) {
+                  topic.maxNum = 2;
+                  offset.fetch([topic], function (err, offsets) {
+                    if (err) {
+                      return 1;//console.error(err);
+                    }
+                    var min = Math.min(offsets[topic.topic][topic.partition]);
+                    consumer.setOffset(topic.topic, topic.partition, min);
+                  });
+                });
+            */
+            return [2 /*return*/, {
+                    status: 200,
+                    body: "hello, world!\n"
+                }];
+        });
+    });
+};
--- kafka-node/kafka-consumer.ts
+++ kafka-node/kafka-consumer.ts
@@ -1,39 +1,53 @@
-var kafka = require('kafka-node');
-var Consumer = kafka.Consumer;
-var Offset = kafka.Offset;
-var Client = kafka.KafkaClient;
-var topic = 'windowsSucks';
+module.exports = async function(context) {
 
-var client = new Client(process.env.ZOOKEEPER_PEERS);
-client.on('ready',function() {
-  console.log('client is ready');
-  console.log(client);
-});
+    var kafka = require('kafka-node');
+    var Consumer = kafka.Consumer;
+    var Offset = kafka.Offset;
+    var Client = kafka.KafkaClient;
+    var topic = 'my-topic';
+    var outcome = 'none';
 
-var topics = [
-    {topic: topic, partition: 0,offset:0},
-];
-var options = { autoCommit: false, fetchMaxWaitMs: 1000,fetchMinBytes: 1, fetchMaxBytes: 1024 * 1024 , fromOffset:true};
+    //console.log('Your typescript works');
+    var client = new Client();         //(process.env.ZOOKEEPER_PEERS);
+    client.on('ready',function() {
+      //console.log('client is ready');
+      //console.log(client);
+      //console.log('End of client');
+    });
 
-var consumer = new Consumer(client, topics, options);
-var offset = new Offset(client);
+    var topics = [
+        {topic: topic, partition: 0,offset:0},
+    ];
+    var options = { autoCommit: false, fetchMaxWaitMs: 1000,fetchMinBytes: 1, fetchMaxBytes: 1024 * 1024 , fromOffset:true};
 
-consumer.on('message', function (message) {
-  console.log(message);
-});
+    var consumer = new Consumer(client, topics, options);
+    var offset = new Offset(client);
 
-consumer.on('error', function (err) {
-  console.log('error', err);
-});
+    //console.log('Something is going on');
+
+    consumer.on('message', function (message) {
+      outcome = message;
+      //console.log(message);
+    });
+
+    consumer.on('error', function (err) {
+      //console.log('error', err);
+    });
 
 
-consumer.on('offsetOutOfRange', function (topic) {
-  topic.maxNum = 2;
-  offset.fetch([topic], function (err, offsets) {
-    if (err) {
-      return console.error(err);
-    }
-    var min = Math.min(offsets[topic.topic][topic.partition]);
-    consumer.setOffset(topic.topic, topic.partition, min);
-  });
-});
+    consumer.on('offsetOutOfRange', function (topic) {
+      topic.maxNum = 2;
+      offset.fetch([topic], function (err, offsets) {
+        if (err) {
+          return 1;//console.error(err);
+        }
+        var min = Math.min(offsets[topic.topic][topic.partition]);
+        consumer.setOffset(topic.topic, topic.partition, min);
+      });
+    });
+
+    return {
+        status: 200,
+        body: "hello, world!\n"
+    };
+}
